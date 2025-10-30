5621a93e0e8e9d38eb55a6228a7a103b2fc6f6c3
Alberto Sclocchi <scloks75tv@gmail.com>
2025-10-29 23:01:17 -0400

    add create checkout session using stripe library

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const Order = require("../models/Order.model");
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 
 
 router.get("/", (req, res, next) => {
@@ -21,6 +23,33 @@ router.post("/", (req, res, next) => {
   .catch((err) =>{
     res.json({success: "false", message: err});
   });
+});
+
+router.post('/create-checkout-session', async (req, res) => {
+  console.log(req.body);
+
+  const lineItems = req.body.map(product => {
+    return {
+      price_data:{
+        currency: 'usd',
+        product_data:{
+          name: product.name,
+        },
+        unit_amount: Math.round(product.price * 100),
+      },      
+      quantity: product.quantity,
+    };
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: 'payment',
+    success_url: `${process.env.ORIGIN}?success=true`,
+    cancel_url: `${process.env.ORIGIN}?success=false`,
+  });
+
+  res.json({ url: session.url });
+
 });
 
 module.exports = router;
